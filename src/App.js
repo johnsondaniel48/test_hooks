7d52ac935ed7e8af7844b95984f54be1c88ee64c
johnsondaniel48 <johnsondaniel48@gmail.com>
2020-07-14 13:01:15 +1000

    Update App.js
    created the sample

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Topic({topic,index}){
+return (
+  <div>
+    {topic.name}
+    {topic.title}
+  </div>
+)
 }
 
+function TopicForm({addTopics}){
+const [name, setName] = useState('');
+
+const handleSubmit=e=> {
+  e.preventDefault();
+  if(!name)
+  return;
+  addTopics(name);
+  setName('');
+}
+
+return (
+<form onSubmit={handleSubmit}>
+<input type="text"  className="input" value={name} onChange={e=>setName(e.target.value)}/>
+<button type="submit">Submit</button>
+</form>
+)
+}
+
+
+function App(){
+  const [topics,setTopics] = useState([
+    {name:"New topic 1",title:"New topic title 1"},
+    {name:"New topic 2",title:"New topic title 2"},
+    {name:"New topic 3",title:"New topic title 3"}
+  ]);
+
+  
+const addTopics=name=>{
+  const updatedTopics= [...topics,{name}];
+  setTopics(updatedTopics);
+}
+
+
+  return(
+    <div className="App">
+      <div>
+          {topics.map((topic,index)=>(
+            <Topic key={index} index={index} topic={topic}/>
+          ))}
+          <TopicForm addTopics={addTopics}/>
+      </div>
+    </div>
+  )
+}
 export default App;
